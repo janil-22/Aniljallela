eb4bf915cc365bbc5d787279fe11f87212cbcae7
janil-22 <78896458+janil-22@users.noreply.github.com>
2021-02-11 14:21:06 +0530

    Create git.py

---
+++ git.py
@@ -0,0 +1,50 @@
+# Headinh1
+##heading2
+###Heading3
+horizental line:
+- - - - - - - - -  - - - - - - -
+**Bold**
+* Italic*
+*** Bold and Italic***
+-text
+*text
+>1.one
+>2.two
+
+link:
+[google](https://github.com/janil-22/Aniljallela/tree/master)
+
+
+code block:
+code`
+
+
+paragraph:
+<p>paragraph</p>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
